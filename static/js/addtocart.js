6db8192079604b518cc0c0ed5e7429425a982877
Reze <112688309+r3ze@users.noreply.github.com>
2023-10-21 11:35:23 +0800

    updated add to cart function

--- static/js/addtocart.js
+++ static/js/addtocart.js
@@ -1,19 +1,43 @@
- // Initialize the cart count
- let cartCount = 0;
 
- // Function to update the cart badge
- function updateCartBadge() {
-     const cartBadge = document.getElementById("cart-badge");
-     cartBadge.textContent = cartCount;
- }
+  // Add a click event listener to the "Add to Cart" button
+  document.querySelectorAll(".add-to-cart").forEach(function (button) {
+    button.addEventListener("click", function () {
+        // Get product data from hidden input fields
+        const productName = this.parentElement.querySelector(".product-name").value;
+        const productPrice = this.parentElement.querySelector(".product-price").value;
 
- // Add a click event listener to all buttons with the class "add-to-cart"
- const addToCartButtons = document.querySelectorAll(".add-to-cart");
- addToCartButtons.forEach(function (button) {
-     button.addEventListener("click", function () {
-         // Increment the cart count
-         cartCount++;
-         // Update the cart badge
-         updateCartBadge();
-     });
- });
+        // Create a new FormData object to send to the server
+        const formData = new FormData();
+        formData.append("product_name", productName);
+        formData.append("product_price", productPrice);
+
+        // Send a POST request to add the product to the cart
+        fetch("/add-to-cart", {
+            method: "POST",
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the server
+            if (data.message === "Product added to cart successfully") {
+                // Update the cart badge by fetching the cart count
+                fetch("/get-cart-count")
+                    .then(response => response.json())
+                    .then(cartData => {
+                        // Update the cart badge with the new count
+                        const cartBadge = document.getElementById("cart-badge");
+                        cartBadge.textContent = cartData.count;
+                    })
+                    .catch(error => {
+                        console.error("Error fetching cart count:", error);
+                    });
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+    });
+});
+
+
+//remove
