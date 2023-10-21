e716d3c226c9d8fff08dfe69890b9f91664481ea
Reze <112688309+r3ze@users.noreply.github.com>
2023-10-21 11:34:02 +0800

    added cart model

--- models.py
+++ models.py
@@ -18,3 +18,24 @@ class accounts(db.Model):
 
     def __repr__(self):
         return f'<Student {self.username}>'
+    
+class cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, nullable=False)
+    seller_id = db.Column(db.Integer, nullable=False)
+    product_name = db.Column(db.String(100), nullable=False)
+    product_image = db.Column(db.LargeBinary, nullable=False)
+    mime_type = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    color = db.Column(db.String(100), )
+    size = db.Column(db.String(100), )
+    price = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Integer, nullable=False)
+
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+
+
+    def __repr__(self):
+        return f'<cart {self.product_name}>'
