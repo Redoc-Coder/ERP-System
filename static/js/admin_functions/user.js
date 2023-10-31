38cf8591d6d11b273e6aa614db6904bc961abc87
Reze <112688309+r3ze@users.noreply.github.com>
2023-10-31 21:55:43 +0800

    added functions for search user

--- static/js/admin_functions/user.js
+++ static/js/admin_functions/user.js
@@ -1,53 +1,20 @@
 function redirectToUserPage() {
 
-  window.location.href = "/user";
+  window.location.href = "/admin/user";
 }
-  const itemsPerPage = 12; // Number of items to show per page
-  let currentPage = 1;
+//search users
+document.getElementById("searchInput").addEventListener("input", function () {
+  const searchTerm = this.value.toLowerCase();
+  const rows = document.querySelectorAll("tbody tr");
 
-  // Function to show/hide rows based on the current page
-  function showPage(pageNumber) {
-    const rows = document.querySelectorAll("tbody tr");
-    const startIndex = (pageNumber - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
+  rows.forEach((row) => {
+    const emailCell = row.querySelector("td:nth-child(3)");
+    const email = emailCell.textContent.toLowerCase();
 
-    rows.forEach((row, index) => {
-      if (index >= startIndex && index < endIndex) {
-        row.classList.remove("hidden");
-      } else {
-        row.classList.add("hidden");
-      }
-    });
-  }
-
-  // Initially, show the first page
-  showPage(currentPage);
-
-  // Handle "Next" and "Previous" button clicks
-  document.getElementById("prevPage").addEventListener("click", () => {
-    if (currentPage > 1) {
-      currentPage--;
-      showPage(currentPage);
+    if (email.startsWith(searchTerm)) {
+      row.style.display = "table-row";
+    } else {
+      row.style.display = "none";
     }
   });
-
-  document.getElementById("nextPage").addEventListener("click", () => {
-    const totalRows = document.querySelectorAll("tbody tr").length;
-    const totalPages = Math.ceil(totalRows / itemsPerPage);
-
-    if (currentPage < totalPages) {
-      currentPage++;
-      showPage(currentPage);
-    }
-  });
-
-  // Handle page number clicks
-  document.getElementById("page1").addEventListener("click", () => {
-    currentPage = 1;
-    showPage(currentPage);
-  });
-
-  document.getElementById("page2").addEventListener("click", () => {
-    currentPage = 2;
-    showPage(currentPage);
-  });
+});
