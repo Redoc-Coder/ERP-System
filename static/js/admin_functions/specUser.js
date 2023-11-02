c35ef83f44002d5532a5c259963e4a43806652cc
Reze <112688309+r3ze@users.noreply.github.com>
2023-11-02 19:03:30 +0800

    incomplete

--- static/js/admin_functions/specUser.js
+++ static/js/admin_functions/specUser.js
@@ -1,1 +1,16 @@
+// get the ID of the user 
 
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("view-more-button")) {
+        const userId = event.target.getAttribute("data-user-id");
+        redirectToUserPage(userId);
+    }
+});
+
+function redirectToUserPage(userId) {
+    // Construct the URL with the user's ID
+    const userPageURL = `/user/${userId}`;
+
+    // Redirect to the user's page
+    window.location.href = userPageURL;
+}
